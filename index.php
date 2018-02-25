6bb2cc8b06f1484d3d24b05fcfbb919f47884d84
Cesar Roberto <cesar.herbas@gmail.com>
2018-02-25 15:55:40 -0400

    Armando el Index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 	include('ConcreteBuilder.php');
+	include('Director.php')
 	
+	$cocina = new CocinaPizza();
+	$pizza_hawai = new HawaiPizzaBuilder();
+	$pizza_hongos = new HongosPizzaBuilder();
+	$pizza_picante = new PicantePizzaBuilder();
+	
+	$cocina -> setPizzaBuilder($pizza_hawai);
+	$cocina -> setPizzaBuilder($pizza_hongos);
+	$cocina -> setPizzaBuilder($pizza_picante);
 	
 ?>
